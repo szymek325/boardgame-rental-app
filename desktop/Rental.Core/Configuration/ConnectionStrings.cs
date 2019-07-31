90f730fe3755c1166d2f52e617e9d6b2c1642a32
Mateusz Szymkowiak <mateusz.x.szymkowiak@gsk.com>
2019-07-31 14:15:43 +0200

    base core project structure added

---
+++ desktop/Rental.Core/Configuration/ConnectionStrings.cs
@@ -0,0 +1,9 @@
+﻿namespace Rental.Core.Configuration
+{
+    public class ConnectionStrings
+    {
+        public bool UseSqLite { get; set; }
+        public string SqlServer { get; set; }
+        public string SqLite { get; set; }
+    }
+}
---
+++ desktop/Rental.Core/Entities/BoardGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Rental.Core.Entities
+{
+    public class BoardGame : BaseEntity
+    {
+        public string Name { get; set; }
+        public float PricePerDay { get; set; }
+        public float SuggestedDeposit { get; set; }
+        public ICollection<GameRental> GameRentals { get; set; }
+    }
+}
