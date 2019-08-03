4dce9f502bdb18c9a4468f7aa41a27ed739547a5
matszym325 <matszym325@gmail.com>
2019-08-03 14:30:13 +0200

    DI added to WPF project

--- desktop/Rental.WPF/App.xaml.cs
+++ desktop/Rental.WPF/App.xaml.cs
@@ -1,17 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rental.Core;
+using Rental.Core.Configuration;
+using Rental.DataAccess;
 
 namespace Rental.WPF
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public IConfiguration Configuration { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environment}.json", true, true);
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient(typeof(MainWindow));
+
+            var connectionStrings = new ConnectionStrings();
+            Configuration.GetSection(nameof(ConnectionStrings)).Bind(connectionStrings);
+            services.AddCoreModule();
+            services.AddDataAccessModule(connectionStrings);
+        }
     }
 }
