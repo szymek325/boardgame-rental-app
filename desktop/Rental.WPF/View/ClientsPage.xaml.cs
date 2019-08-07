9b0c0844fabc7de8df727245e0dd5873066fe0f7
Mateusz Szymkowiak <mateusz.x.szymkowiak@gsk.com>
2019-08-07 08:33:30 +0200

    desktop- navigation menu

--- desktop/Rental.WPF/View/ClientsPage.xaml.cs
+++ desktop/Rental.WPF/View/ClientsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace Rental.WPF.View
 {
@@ -11,6 +12,16 @@ namespace Rental.WPF.View
         public ClientsPage()
         {
             InitializeComponent();
+        }
+
+        void NavigationService_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            // Don't allow refreshing of a static page
+            if ((e.NavigationMode == NavigationMode.Refresh) &&
+                (e.Uri.OriginalString == "StaticPage.xaml"))
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
---
+++ desktop/Rental.WPF/View/MenuPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Rental.Core.Interfaces.DataAccess;
+
+namespace Rental.WPF.View
+{
+    /// <summary>
+    ///     Interaction logic for MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        private readonly ClientsPage _clientsPage;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MenuPage(ClientsPage clientsPage, IUnitOfWork unitOfWork)
+        {
+            InitializeComponent();
+            _clientsPage = clientsPage;
+            _unitOfWork = unitOfWork;
+        }
+
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToRentals(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GoToClients(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(new ClientsPage());
+        }
+
+        private void GoToGames(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
