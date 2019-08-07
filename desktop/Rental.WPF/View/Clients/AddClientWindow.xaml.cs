23e97e610410236d56097900c7c438ec2939531a
Mateusz Szymkowiak <mateusz.x.szymkowiak@gsk.com>
2019-08-07 13:20:20 +0200

    desktop-working view model for addClient

---
+++ desktop/Rental.WPF/View/Clients/AddClientWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Rental.WPF.ViewModel;
+using Rental.WPF.ViewModel.Clients;
+
+namespace Rental.WPF.View.Clients
+{
+    /// <summary>
+    ///     Interaction logic for AddClientWindow.xaml
+    /// </summary>
+    public partial class AddClientWindow : Window
+    {
+        private readonly AddClientViewModel _viewModel;
+
+        public AddClientWindow(AddClientViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+        public AddClientWindow()
+        {
+            InitializeComponent();
+            DataContext = new AddClientViewModel();
+        }
+    }
+}
