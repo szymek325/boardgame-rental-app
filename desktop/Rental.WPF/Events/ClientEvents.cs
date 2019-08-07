f78d3299d9515a79b233d5e67572b1e5712fb0a9
matszym325 <matszym325@gmail.com>
2019-08-07 21:56:10 +0200

    desktop- clientEvents created

---
+++ desktop/Rental.WPF/Events/ClientEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Rental.Core.Entities;
+
+namespace Rental.WPF.Events
+{
+    public static class ClientEvents
+    {
+        public static event EventHandler<Client> OnNewClientAdded;
+
+        public static void RaiseOnNewClientAdded(object sender, Client client)
+        {
+            OnNewClientAdded?.Invoke(sender, client);
+        }
+    }
+}
