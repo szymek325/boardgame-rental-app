994acf4669406a7c57084d2bd86d566436be37f5
matszym325 <matszym325@gmail.com>
2019-08-07 23:02:32 +0200

    desktop- open specific client page on double click

--- desktop/Rental.WPF/View/Clients/ClientPage.xaml.cs
+++ desktop/Rental.WPF/View/Clients/ClientPage.xaml.cs
@@ -1,26 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Rental.Core.Entities;
 
 namespace Rental.WPF.View.Clients
 {
     /// <summary>
-    /// Interaction logic for ClientPage.xaml
+    ///     Interaction logic for ClientPage.xaml
     /// </summary>
-    public partial class ClientPage : Page
+    public partial class ClientPage : Window
     {
-        public ClientPage()
+        public ClientPage(Client client)
         {
             InitializeComponent();
+            DataContext = client;
         }
     }
 }
