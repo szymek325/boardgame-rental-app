2b67dd11f7fd8275281e2b91c2979b3dc348be80
matszym325 <matszym325@gmail.com>
2019-08-09 00:01:19 +0200

    mediatr- more commands for clients added

---
+++ desktop/Rental.Core/Requests/Handlers/RemoveClientRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Rental.Core.Requests.Handlers
+{
+    internal class RemoveClientRequestHandler : IRequestHandler<RemoveClientRequest, string>
+    {
+        private readonly IMediator _mediator;
+
+        public RemoveClientRequestHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<string> Handle(RemoveClientRequest request, CancellationToken cancellationToken)
+        {
+            var client = await _mediator.Send(new GetClientByIdRequest(request.Id), cancellationToken);
+            //var anyOpen
+            //TODO
+            return String.Empty;;
+        }
+    }
+}
