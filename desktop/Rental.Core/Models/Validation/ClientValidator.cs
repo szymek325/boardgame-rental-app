f896419e369e16efbb4db56173e98d0acffb80c4
Mateusz Szymkowiak <mateusz.x.szymkowiak@gsk.com>
2019-08-08 13:40:57 +0200

    desktop-layers - fluent validation for phone number added

---
+++ desktop/Rental.Core/Models/Validation/ClientValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Rental.Core.Extensions;
+
+namespace Rental.Core.Models.Validation
+{
+    internal class ClientValidator : AbstractValidator<Client>
+    {
+        public ClientValidator()
+        {
+            RuleFor(x => x.FirstName).Length(2, 30);
+            RuleFor(x => x.LastName).Length(2, 30);
+            RuleFor(x => x.EmailAddress).EmailAddress();
+            RuleFor(x => x.ContactNumber).ContactNumber();
+        }
+    }
+}
