92a61298de8b67c15c637c569ea8177fef6280f8
matszym325 <matszym325@gmail.com>
2019-08-09 00:01:19 +0200

    mediatr- more commands for clients added

---
+++ desktop/Rental.Core/Requests/GetClientByIdRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MediatR;
+using Rental.Core.Models;
+
+namespace Rental.Core.Requests.Handlers
+{
+    public class GetClientByIdRequest : IRequest<Client>
+    {
+        public GetClientByIdRequest(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
