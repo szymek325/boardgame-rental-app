dc35f1a0c470c63cec29bec8b0795f2363aadc3d
matszym325 <matszym325@gmail.com>
2019-08-10 11:27:57 +0200

    add requests input updated

--- desktop/Rental.Core/Models/BoardGame.cs
+++ desktop/Rental.Core/Models/BoardGame.cs
@@ -4,6 +4,17 @@ namespace Rental.Core.Models
 {
     public class BoardGame
     {
+        public BoardGame()
+        {
+        }
+
+        public BoardGame(string name, float price)
+        {
+            Name = name;
+            Price = price;
+            CreationTime=DateTime.UtcNow;
+        }
+
         public Guid Id { get; set; }
         public string Name { get; set; }
         public float Price { get; set; }
--- desktop/Rental.Core/Requests/AddClientRequest.cs
+++ desktop/Rental.Core/Requests/AddClientRequest.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using MediatR;
-using Rental.Core.Models;
 
 namespace Rental.Core.Requests
 {
     public class AddClientRequest : IRequest<Guid>
     {
-        public AddClientRequest(Client client)
+        public AddClientRequest(string firstName, string lastName, string contactNumber, string emailAddress)
         {
-            Client = client;
+            FirstName = firstName;
+            LastName = lastName;
+            ContactNumber = contactNumber;
+            EmailAddress = emailAddress;
         }
 
-        public Client Client { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
