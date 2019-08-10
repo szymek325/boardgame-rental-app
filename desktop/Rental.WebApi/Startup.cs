195bf45d41f8024e383e7397b680c1580d85c7be
matszym325 <matszym325@gmail.com>
2019-08-10 20:22:07 +0200

    web api project added

---
+++ desktop/Rental.WebApi/Startup.cs
@@ -0,0 +1,50 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Rental.Core;
+using Rental.Core.Configuration;
+using Rental.DataAccess;
+
+namespace Rental.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAutoMapper(typeof(Startup), typeof(CoreModule), typeof(EntityFrameworkModule));
+            services.AddMediatR(typeof(Startup), typeof(CoreModule), typeof(EntityFrameworkModule));
+
+            var connectionStrings = new ConnectionStrings();
+            Configuration.GetSection(nameof(ConnectionStrings)).Bind(connectionStrings);
+            services.AddCoreModule();
+            services.AddDataAccessModule(connectionStrings);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
