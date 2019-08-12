b912a2215286f575e789fba107ed6a7a9e8af937
matszym325 <matszym325@gmail.com>
2019-08-12 06:36:46 +0200

    rentals queries and commands input prepared + validator

---
+++ desktop/Rental.Core/Interfaces/DataAccess/GameRentalRequests/GetRentalByIdRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MediatR;
+using Rental.Core.Models;
+
+namespace Rental.Core.Interfaces.DataAccess.GameRentalRequests
+{
+    public class GetRentalByIdRequest : IRequest<GameRental>
+    {
+        public GetRentalByIdRequest(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ desktop/Rental.Core/Interfaces/DataAccess/GameRentalRequests/UpdateAndSaveGameRentalNotification.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Rental.Core.Models;
+
+namespace Rental.Core.Interfaces.DataAccess.GameRentalRequests
+{
+    public class UpdateAndSaveGameRentalNotification : INotification
+    {
+        public UpdateAndSaveGameRentalNotification(GameRental gameRental)
+        {
+            GameRental = gameRental;
+        }
+
+        public GameRental GameRental { get; set; }
+    }
+}
