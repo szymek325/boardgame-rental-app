470102063f55e8a6071a4efc22c62014680fcf94
szymek325 <matszym325@gmail.com>
2019-08-12 18:10:13 +0200

    more tests with inmemory and real mapper

---
+++ desktop/Rental.DataAccess.Tests/BoardGameHandlers/CheckIfBoardGameHasOnlyCompletedRentalsRequestHandlerTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Rental.Core.Interfaces.DataAccess.BoardGameRequests;
+using Rental.Core.Models;
+using Rental.DataAccess.Context;
+using Rental.DataAccess.Handlers.BoardGameHandlers;
+using Xunit;
+using GameRental = Rental.DataAccess.Entities.GameRental;
+
+namespace Rental.DataAccess.Tests.BoardGameHandlers
+{
+    public class CheckIfBoardGameHasOnlyCompletedRentalsRequestHandlerTests
+    {
+        public CheckIfBoardGameHasOnlyCompletedRentalsRequestHandlerTests()
+        {
+            _rentalContext = new RentalContext(new DbContextOptionsBuilder<RentalContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options);
+            _sut = new CheckIfBoardGameHasOnlyCompletedRentalsRequestHandler(_rentalContext);
+        }
+
+        private readonly RentalContext _rentalContext;
+        private readonly IRequestHandler<CheckIfBoardGameHasOnlyCompletedRentalsRequest, bool> _sut;
+
+        [Fact]
+        public async Task Handle_Should_ReturnFalse_When_ThereIsAnInProgressRental()
+        {
+            var boardGameId = Guid.NewGuid();
+            var input = new CheckIfBoardGameHasOnlyCompletedRentalsRequest(boardGameId);
+            var rentals = new List<GameRental>
+            {
+                new GameRental
+                {
+                    BoardGameId = boardGameId,
+                    Status = Status.InProgress
+                },
+                new GameRental
+                {
+                    BoardGameId = boardGameId,
+                    Status = Status.Completed
+                }
+            };
+            await _rentalContext.GameRentals.AddRangeAsync(rentals);
+            await _rentalContext.SaveChangesAsync();
+
+            var response = await _sut.Handle(input, new CancellationToken());
+
+            response.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnTrue_When_RentalsTableIsEmpty()
+        {
+            var boardGameId = Guid.NewGuid();
+            var input = new CheckIfBoardGameHasOnlyCompletedRentalsRequest(boardGameId);
+
+            var response = await _sut.Handle(input, new CancellationToken());
+
+            response.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnTrue_When_ThereAreOnlyCompletedRentals()
+        {
+            var boardGameId = Guid.NewGuid();
+            var input = new CheckIfBoardGameHasOnlyCompletedRentalsRequest(boardGameId);
+            var rentals = new List<GameRental>
+            {
+                new GameRental
+                {
+                    BoardGameId = boardGameId,
+                    Status = Status.Completed
+                },
+                new GameRental
+                {
+                    BoardGameId = boardGameId,
+                    Status = Status.Completed
+                }
+            };
+            await _rentalContext.GameRentals.AddRangeAsync(rentals);
+            await _rentalContext.SaveChangesAsync();
+
+            var response = await _sut.Handle(input, new CancellationToken());
+
+            response.Should().BeTrue();
+        }
+    }
+}
---
+++ desktop/Rental.DataAccess.Tests/BoardGameHandlers/GetAllBoardGamesRequestHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Rental.Core.Interfaces.DataAccess.BoardGameRequests;
+using Rental.Core.Models;
+using Rental.DataAccess.Context;
+using Rental.DataAccess.Handlers.BoardGameHandlers;
+using Rental.DataAccess.Mapping;
+using Xunit;
+
+namespace Rental.DataAccess.Tests.BoardGameHandlers
+{
+    public class GetAllBoardGamesRequestHandlerTests
+    {
+        public GetAllBoardGamesRequestHandlerTests()
+        {
+            _rentalContext = new RentalContext(new DbContextOptionsBuilder<RentalContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options);
+            _mapper = new Mapper(new MapperConfiguration(cfg => { cfg.AddProfile<EntitiesMapping>(); }));
+            _sut = new GetAllBoardGamesRequestHandler(_mapper, _rentalContext);
+        }
+
+        private readonly IMapper _mapper;
+        private readonly RentalContext _rentalContext;
+        private readonly IRequestHandler<GetAllBoardGamesRequest, IList<BoardGame>> _sut;
+
+        [Fact]
+        public async Task Handle_Should_ReturnMappedListOfGameBoards_When_SomeExist()
+        {
+            var entity1 = new Entities.BoardGame
+            {
+                Id = Guid.NewGuid()
+            };
+            var entity2 = new Entities.BoardGame
+            {
+                Id = Guid.NewGuid()
+            };
+            var entities = new List<Entities.BoardGame> {entity1, entity2};
+            await _rentalContext.BoardGames.AddRangeAsync(entities);
+            await _rentalContext.SaveChangesAsync();
+
+            var result = await _sut.Handle(new GetAllBoardGamesRequest(), new CancellationToken());
+
+            result.Should().Contain(x => x.Id == entity1.Id);
+            result.Should().Contain(x => x.Id == entity2.Id);
+        }
+    }
+}
