f1a876184b4a4f63e5a5b912c7be73b14befe76e
szymek325 <matszym325@gmail.com>
2019-08-18 13:17:45 +0200

    clients controller created

---
+++ src/Rental.WebApi/Dto/CreateBoardGameDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Rental.WebApi.Dto
+{
+    public class CreateBoardGameDto
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ src/Rental.WebApi/Dto/UpdateClientDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Rental.WebApi.Dto
+{
+    public class UpdateClientDto
+    {
+        public Guid ClientGuid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
