0c1471af4129184819949dc659a7e0dd8166e2f6
szymek325 <matszym325@gmail.com>
2020-03-03 14:22:28 +0100

    Refactor RemoveBoardGameCommand

--- tests/Rental.Core.Tests/BoardGames/Commands/RemoveBoardGameCommandHandlerTests.cs
+++ tests/Rental.Core.Tests/BoardGames/Commands/RemoveBoardGameCommandHandlerTests.cs
@@ -4,10 +4,9 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
 using Playingo.Application.BoardGames.Commands;
-using Playingo.Application.BoardGames.Queries;
 using Playingo.Application.Common.Exceptions;
+using Playingo.Application.Common.Interfaces;
 using Playingo.Application.Common.Mediator;
-using Playingo.Application.Interfaces.DataAccess.Commands;
 using Playingo.Domain.BoardGames;
 using Xunit;
 
@@ -17,44 +16,43 @@ namespace Rental.Core.Tests.BoardGames.Commands
     {
         public RemoveBoardGameCommandHandlerTests()
         {
-            _mediatorService = new Mock<IMediatorService>(MockBehavior.Strict);
-            _sut = new RemoveBoardGameCommandHandler(_mediatorService.Object);
+            _unitOfWork = new Mock<IUnitOfWork>(MockBehavior.Strict);
+            _sut = new RemoveBoardGameCommandHandler(_unitOfWork.Object);
         }
 
         private readonly CancellationToken _cancellationToken = new CancellationToken();
         private readonly RemoveBoardGameCommand _inputCommand = new RemoveBoardGameCommand(Guid.NewGuid());
 
-        private readonly Mock<IMediatorService> _mediatorService;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
         private readonly ICommandHandler<RemoveBoardGameCommand> _sut;
 
         [Fact]
         public async Task Handle_Should_RemoveAndSaveBoardGame_When_GameExistAndCanBeRemoved()
         {
             var boardGame = new BoardGame(_inputCommand.Id, "name", 15);
-            _mediatorService
-                .Setup(x => x.Send(It.Is((GetBoardGameByIdQuery q) => q.Id == _inputCommand.Id), _cancellationToken))
+            _unitOfWork
+                .Setup(x => x.BoardGameRepository.GetByIdAsync(_inputCommand.Id, _cancellationToken))
                 .ReturnsAsync(boardGame);
-            _mediatorService
-                .Setup(x => x.Send(It.Is((CheckIfBoardGameHasOnlyCompletedRentalsQuery q) => q.Id == _inputCommand.Id),
-                    _cancellationToken))
+            _unitOfWork
+                .Setup(x => x.RentalRepository.AreAllCompletedForBoardGameAsync(boardGame.Id, _cancellationToken))
                 .ReturnsAsync(true);
-            _mediatorService
-                .Setup(x => x.Send(It.Is((RemoveAndSaveBoardGameByIdCommand c) => c.Id == _inputCommand.Id),
-                    _cancellationToken)).Returns(Task.CompletedTask);
+            _unitOfWork.Setup(x => x.BoardGameRepository.RemoveByIdAsync(boardGame.Id, _cancellationToken))
+                .Returns(Task.CompletedTask);
+            _unitOfWork.Setup(x => x.SaveChangesAsync(_cancellationToken)).Returns(Task.CompletedTask);
 
             await _sut.Handle(_inputCommand, _cancellationToken);
 
-            _mediatorService.Verify(x =>
-                x.Send(It.Is((RemoveAndSaveBoardGameByIdCommand c) => c.Id == _inputCommand.Id),
-                    _cancellationToken), Times.Once);
+            _unitOfWork.Verify(x => x.BoardGameRepository.RemoveByIdAsync(boardGame.Id, _cancellationToken),
+                Times.Once);
+            _unitOfWork.Verify(x => x.SaveChangesAsync(_cancellationToken), Times.Once);
         }
 
         [Fact]
         public void Handle_Should_ThrowBoardGameHasOpenRentalException_When_BoardGameWasNotFoundInDb()
         {
             BoardGame boardGame = null;
-            _mediatorService
-                .Setup(x => x.Send(It.Is((GetBoardGameByIdQuery q) => q.Id == _inputCommand.Id), _cancellationToken))
+            _unitOfWork
+                .Setup(x => x.BoardGameRepository.GetByIdAsync(_inputCommand.Id, _cancellationToken))
                 .ReturnsAsync(boardGame);
 
             Func<Task> act = async () => await _sut.Handle(_inputCommand, _cancellationToken);
@@ -66,12 +64,11 @@ namespace Rental.Core.Tests.BoardGames.Commands
         public void Handle_Should_ThrowBoardGameHasOpenRentalException_When_GameHasRentalsInProgress()
         {
             var boardGame = new BoardGame(_inputCommand.Id, "name", 15);
-            _mediatorService
-                .Setup(x => x.Send(It.Is((GetBoardGameByIdQuery q) => q.Id == _inputCommand.Id), _cancellationToken))
+            _unitOfWork
+                .Setup(x => x.BoardGameRepository.GetByIdAsync(_inputCommand.Id, _cancellationToken))
                 .ReturnsAsync(boardGame);
-            _mediatorService
-                .Setup(x => x.Send(It.Is((CheckIfBoardGameHasOnlyCompletedRentalsQuery q) => q.Id == _inputCommand.Id),
-                    _cancellationToken))
+            _unitOfWork
+                .Setup(x => x.RentalRepository.AreAllCompletedForBoardGameAsync(boardGame.Id, _cancellationToken))
                 .ReturnsAsync(false);
 
             Func<Task> act = async () => await _sut.Handle(_inputCommand, _cancellationToken);
