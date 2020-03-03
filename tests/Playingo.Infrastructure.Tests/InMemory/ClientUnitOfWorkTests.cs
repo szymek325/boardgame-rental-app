36b48b4969a60228edea321186908538c73f4e25
szymek325 <matszym325@gmail.com>
2020-03-03 16:40:29 +0100

    Move queries tests to UnitOfWorkTests

---
+++ tests/Playingo.Infrastructure.Tests/InMemory/ClientUnitOfWorkTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Playingo.Application.Common.Interfaces;
+using Playingo.Infrastructure.Persistence.Context;
+using Playingo.Infrastructure.Persistence.Entities;
+using Playingo.Infrastructure.Persistence.Mapping;
+using Playingo.Infrastructure.Persistence.Repositories;
+using Xunit;
+
+namespace Playingo.Infrastructure.Tests.InMemory
+{
+    public class ClientUnitOfWorkTests
+    {
+        public ClientUnitOfWorkTests()
+        {
+            var contextOptions = new DbContextOptionsBuilder<PlayingoContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg => { cfg.AddProfile<EntitiesMapping>(); }));
+            _playingoContext = new PlayingoContext(contextOptions);
+            _sut = new UnitOfWork(_playingoContext, mapper);
+        }
+
+        private readonly PlayingoContext _playingoContext;
+        private readonly IUnitOfWork _sut;
+        private readonly CancellationToken _cancellationToken = CancellationToken.None;
+
+        [Fact]
+        public async Task GetAllAsync_Should_ReturnEmptyListOfClients_When_ClientsTableIsEmpty()
+        {
+            var result = await _sut.ClientRepository.GetAllAsync(_cancellationToken);
+
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetAllAsync_Should_ReturnMappedListOfClients_When_SomeExist()
+        {
+            var entity1 = new Client
+            {
+                Id = Guid.NewGuid()
+            };
+            var entity2 = new Client
+            {
+                Id = Guid.NewGuid()
+            };
+            var entities = new List<Client> {entity1, entity2};
+            await _playingoContext.Clients.AddRangeAsync(entities, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var result = await _sut.ClientRepository.GetAllAsync(_cancellationToken);
+
+            result.Should().Contain(x => x.Id == entity1.Id);
+            result.Should().Contain(x => x.Id == entity2.Id);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_Should_ReturnElementWithSpecificId()
+        {
+            var inputId = Guid.NewGuid();
+            var entity1 = new Client
+            {
+                Id = inputId,
+                FirstName = "test1"
+            };
+            var entity2 = new Client
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "test2"
+            };
+            var entities = new List<Client> {entity1, entity2};
+            await _playingoContext.Clients.AddRangeAsync(entities, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var result = await _sut.ClientRepository.GetByIdAsync(inputId, _cancellationToken);
+
+            result.Id.Should().Be(inputId);
+            result.FirstName.Should().Be(entity1.FirstName);
+            result.CreationTime.Should().Be(entity1.CreationTime);
+            result.Should().BeOfType<Domain.Clients.Client>();
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_Should_ReturnNull_When_NoMatchingElementsFound()
+        {
+            var entity1 = new Client
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "test1"
+            };
+            var entity2 = new Client
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "test2"
+            };
+            var entities = new List<Client> {entity1, entity2};
+            await _playingoContext.Clients.AddRangeAsync(entities, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var result = await _sut.ClientRepository.GetByIdAsync(Guid.NewGuid(), _cancellationToken);
+
+            result.Should().BeNull();
+        }
+    }
+}
---
+++ tests/Playingo.Infrastructure.Tests/InMemory/RentalUnitOfWorkTests.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Playingo.Application.Common.Interfaces;
+using Playingo.Domain;
+using Playingo.Infrastructure.Persistence.Context;
+using Playingo.Infrastructure.Persistence.Entities;
+using Playingo.Infrastructure.Persistence.Mapping;
+using Playingo.Infrastructure.Persistence.Repositories;
+using Xunit;
+
+namespace Playingo.Infrastructure.Tests.InMemory
+{
+    public class RentalUnitOfWorkTests
+    {
+        public RentalUnitOfWorkTests()
+        {
+            var contextOptions = new DbContextOptionsBuilder<PlayingoContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg => { cfg.AddProfile<EntitiesMapping>(); }));
+            _playingoContext = new PlayingoContext(contextOptions);
+            _sut = new UnitOfWork(_playingoContext, mapper);
+        }
+
+        private readonly PlayingoContext _playingoContext;
+        private readonly IUnitOfWork _sut;
+        private readonly CancellationToken _cancellationToken = CancellationToken.None;
+
+        [Fact]
+        public async Task AreAllCompletedForBoardGameAsync_Should_ReturnFalse_When_ThereIsAnInProgressRental()
+        {
+            var boardGameId = Guid.NewGuid();
+            var rentals = new List<Rental>
+            {
+                new Rental
+                {
+                    BoardGameId = boardGameId,
+                    Status = Status.InProgress
+                },
+                new Rental
+                {
+                    BoardGameId = boardGameId,
+                    Status = Status.Completed
+                }
+            };
+            await _playingoContext.Rentals.AddRangeAsync(rentals, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var response =
+                await _sut.RentalRepository.AreAllCompletedForBoardGameAsync(boardGameId, _cancellationToken);
+
+            response.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task AreAllCompletedForBoardGameAsync_Should_ReturnTrue_When_RentalsTableIsEmpty()
+        {
+            var boardGameId = Guid.NewGuid();
+
+            var response =
+                await _sut.RentalRepository.AreAllCompletedForBoardGameAsync(boardGameId, _cancellationToken);
+
+            response.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task AreAllCompletedForBoardGameAsync_Should_ReturnTrue_When_ThereAreOnlyCompletedRentals()
+        {
+            var boardGameId = Guid.NewGuid();
+            var rentals = new List<Rental>
+            {
+                new Rental
+                {
+                    BoardGameId = boardGameId,
+                    Status = Status.Completed
+                },
+                new Rental
+                {
+                    BoardGameId = boardGameId,
+                    Status = Status.Completed
+                }
+            };
+            await _playingoContext.Rentals.AddRangeAsync(rentals, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var response =
+                await _sut.RentalRepository.AreAllCompletedForBoardGameAsync(boardGameId, _cancellationToken);
+
+            response.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task AreAllCompletedForClientAsync_Should_ReturnFalse_When_ClientHasInProgressRental()
+        {
+            var clientId = Guid.NewGuid();
+            var rentals = new List<Rental>
+            {
+                new Rental
+                {
+                    ClientId = clientId,
+                    Status = Status.InProgress
+                },
+                new Rental
+                {
+                    ClientId = clientId,
+                    Status = Status.Completed
+                }
+            };
+            await _playingoContext.Rentals.AddRangeAsync(rentals, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var response = await _sut.RentalRepository.AreAllCompletedForClientAsync(clientId, _cancellationToken);
+
+            response.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task AreAllCompletedForClientAsync_Should_ReturnTrue_When_RentalsTableIsEmpty()
+        {
+            var clientId = Guid.NewGuid();
+
+            var response = await _sut.RentalRepository.AreAllCompletedForClientAsync(clientId, _cancellationToken);
+
+            response.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task AreAllCompletedForClientAsync_Should_ReturnTrue_When_ThereAreOnlyCompletedRentals()
+        {
+            var clientId = Guid.NewGuid();
+
+            var rentals = new List<Rental>
+            {
+                new Rental
+                {
+                    ClientId = clientId,
+                    Status = Status.Completed
+                },
+                new Rental
+                {
+                    ClientId = clientId,
+                    Status = Status.Completed
+                }
+            };
+            await _playingoContext.Rentals.AddRangeAsync(rentals, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var response = await _sut.RentalRepository.AreAllCompletedForClientAsync(clientId, _cancellationToken);
+
+            response.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task GetAllAsync_Should_ReturnEmptyListOfGameRentals_When_TableIsEmpty()
+        {
+            var result = await _sut.RentalRepository.GetAllAsync(_cancellationToken);
+
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetAllAsync_Should_ReturnMappedListOfGameRentals_When_TheyHaveCorrectBoardGameId()
+        {
+            var inputBoardGameIdGuid = Guid.NewGuid();
+            var entity1 = new Rental
+            {
+                Id = Guid.NewGuid(),
+                BoardGameId = inputBoardGameIdGuid
+            };
+            var entity2 = new Rental
+            {
+                Id = Guid.NewGuid(),
+                ClientId = inputBoardGameIdGuid
+            };
+            var entities = new List<Rental> {entity1, entity2};
+            await _playingoContext.Rentals.AddRangeAsync(entities, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var result = await _sut.RentalRepository.GetAllAsync(_cancellationToken);
+
+            result.Should().BeOfType<List<Domain.Rentals.Rental>>();
+            result.Count.Should().Be(entities.Count);
+        }
+
+        [Fact]
+        public async Task GetAllForBoardGameAsync_Should_ReturnEmptyListOfGameRentals_When_TableIsEmpty()
+        {
+            var input = Guid.NewGuid();
+
+            var result = await _sut.RentalRepository.GetAllForBoardGameAsync(input, _cancellationToken);
+
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetAllForBoardGameAsync_Should_ReturnMappedListOfGameRentals_When_TheyHaveCorrectBoardGameId()
+        {
+            var inputBoardGameIdGuid = Guid.NewGuid();
+            var entity1 = new Rental
+            {
+                Id = Guid.NewGuid(),
+                BoardGameId = inputBoardGameIdGuid
+            };
+            var entity2 = new Rental
+            {
+                Id = Guid.NewGuid(),
+                ClientId = inputBoardGameIdGuid
+            };
+            var entities = new List<Rental> {entity1, entity2};
+            await _playingoContext.Rentals.AddRangeAsync(entities, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var result = await _sut.RentalRepository.GetAllForBoardGameAsync(inputBoardGameIdGuid, _cancellationToken);
+
+            result.Should().BeOfType<List<Domain.Rentals.Rental>>();
+            result.Should().Contain(x => x.Id == entity1.Id);
+            result.Should().NotContain(x => x.Id == entity2.Id);
+            result.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task GetAllForClientAsync_Should_ReturnEmptyListOfGameRentals_When_TableIsEmpty()
+        {
+            var input = Guid.NewGuid();
+
+            var result = await _sut.RentalRepository.GetAllForClientAsync(input, _cancellationToken);
+
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetAllForClientAsync_Should_ReturnMappedListOfGameRentals_When_TheyHaveCorrectBoardGameId()
+        {
+            var inputClientId = Guid.NewGuid();
+            var entity1 = new Rental
+            {
+                Id = Guid.NewGuid(),
+                BoardGameId = inputClientId
+            };
+            var entity2 = new Rental
+            {
+                Id = Guid.NewGuid(),
+                ClientId = inputClientId
+            };
+            var entities = new List<Rental> {entity1, entity2};
+            await _playingoContext.Rentals.AddRangeAsync(entities, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var result = await _sut.RentalRepository.GetAllForClientAsync(inputClientId, _cancellationToken);
+
+            result.Should().BeOfType<List<Domain.Rentals.Rental>>();
+            result.Should().NotContain(x => x.Id == entity1.Id);
+            result.Should().Contain(x => x.Id == entity2.Id);
+            result.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_Should_ReturnElementWithSpecificId()
+        {
+            var inputId = Guid.NewGuid();
+            var entity1 = new Rental
+            {
+                Id = inputId,
+                PaidMoney = 10
+            };
+            var entity2 = new Rental
+            {
+                Id = Guid.NewGuid(),
+                PaidMoney = 20
+            };
+            var entities = new List<Rental> {entity1, entity2};
+            await _playingoContext.Rentals.AddRangeAsync(entities, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var result = await _sut.RentalRepository.GetByIdAsync(inputId, _cancellationToken);
+
+            result.Id.Should().Be(inputId);
+            result.PaidMoney.Should().Be(entity1.PaidMoney);
+            result.Should().BeOfType<Domain.Rentals.Rental>();
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_Should_ReturnNull_When_NoMatchingElementsFound()
+        {
+            var entity1 = new Rental
+            {
+                Id = Guid.NewGuid(),
+                PaidMoney = 10
+            };
+            var entity2 = new Rental
+            {
+                Id = Guid.NewGuid(),
+                PaidMoney = 20
+            };
+            var entities = new List<Rental> {entity1, entity2};
+            await _playingoContext.Rentals.AddRangeAsync(entities, _cancellationToken);
+            await _playingoContext.SaveChangesAsync(_cancellationToken);
+
+            var result = await _sut.RentalRepository.GetByIdAsync(Guid.NewGuid(), _cancellationToken);
+
+            result.Should().BeNull();
+        }
+    }
+}
